ba886cf6f7ef9eb8afb80f096d614d20342176c3
puancoffee <puanngopi@gmail.com>
2020-04-01 14:17:16 +0700

    add data

---
+++ Data.js
@@ -0,0 +1,37 @@
+const DATA = {
+    "employees":[
+      {"firstName":"John", "lastName":"Doe"}, 
+      {"firstName":"Anna", "lastName":"Smith"}, 
+      {"firstName":"Peter", "lastName":"Jones"},
+      {"firstName":"Doni", "lastName":"Wong"}, 
+      {"firstName":"Slamet", "lastName":"Jordan"}, 
+      {"firstName":"Carlo", "lastName":"wee"}, 
+      {"firstName":"philips", "lastName":"morris"},
+      {"firstName":"angga", "lastName":"dwi"}, 
+      {"firstName":"vicky", "lastName":"sasongko"},
+      {"firstName":"nadya", "lastName":"lembayung"} 
+    ],
+    "jobs": [
+        {"department": "QC", "title": "QA"},
+        {"department": "IT", "title": ['IT Support', 'Software engineer', 'UI/UX', 'DevOps', 'Tester']},
+        {"department": "Human Resources Development", "title": "SPV"},
+        {"department": "Finance and Accounting", "title": "Finance"},
+        {"department": "Marketing dan Customer Service", "title": "marketing"},
+        {"department": "Maintenance and Facility", "title": "maintenance"},
+    ],
+    "details": [
+        {"age": [22, 23, 24, ,29, 30, 32 ]},
+        {"experience": ['2 Tahun', '3 Tahun', '4 Tahun',]}
+    ]
+    }
+
+    // name :jhon doe, jobs: department: IT, title: UI/UX, age: 23, experience: 2 Tahun
+    //name :anna smith, jobs: department: Human Resources Development, title: SPV, age: 29, experience: 4 Tahun
+    //name :Peter jhon, jobs: department: IT, title: DevOps, age: 24, experience: 3 Tahun
+    //name :doni wong, jobs: department: IT, title: Tester, age: 22, experience: 2 Tahun
+    //name :slamet jordan, jobs: department: IT, title: UI/UX, age: 23, experience: 2 Tahun
+    //name :carlo wee, jobs: department: Finance and Accounting, title: Finance, age: 32, experience: 4 Tahun
+    //name :philips morris, jobs: department: Maintenance and Facility, title: maintenance, age: 29, experience: 2 Tahun
+    //name :angga dwi, jobs: department: IT, title: IT Support, age: 23, experience: 2 Tahun
+    //name :vicky sasongko, jobs: department: QC, title: QA, age: 24, experience: 2 Tahun
+    //name :nadya lembayung, jobs: department: IT, title: Software engineer, age: 23, experience: 2 Tahun
